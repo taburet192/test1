d913eb60cc9dd909138b8de56d9b83b4c4bc33fe
AlᎥce <100071513+rat1alisa@users.noreply.github.com>
2025-03-24 18:55:49 +0300

    test1full6

--- test1.cpp
+++ test1.cpp
@@ -1,13 +1,25 @@
 #include "iostream"
 
 
+
+void mult(int a, int b)
+{
+	
+	std::cout << a * b << "\n";
+}
+int sum(int a, int b)
+{
+
+	std::cout << a + b << "\n";
+
+}
 int main()
 {
 	int a;
 	int b;
 	std::cin >> a >> b;
-	std::cout << a + b << "\n";
-	mult();
+	sum(a, b);
+	mult(a,b);
 
 
 
