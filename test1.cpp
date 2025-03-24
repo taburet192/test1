27690f83cd357194b2b1da0c9a7d16726eb5042d
AlᎥce <100071513+rat1alisa@users.noreply.github.com>
2025-03-24 18:16:24 +0300

    test1full

--- test1.cpp
+++ test1.cpp
@@ -1,13 +1,19 @@
 #include "iostream"
 
+void mult(int a, int b)
+{
+	std::cout << a * b << "\n";
 
+
+
+}
 int main()
 {
 	int a;
 	int b;
 	std::cin >> a >> b;
 	std::cout << a + b << "\n";
-
+	mult(a, b);
 
 
 
